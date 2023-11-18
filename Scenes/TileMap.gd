fcdd729c058023cb62c375470b69ffcd16f8890b
jtkardon <jtkardon@gmail.com>
2023-11-18 17:20:23 -0500

    fixed vent areas not working??
    idk why they were'nt working they were working yesterday and there were no changes but they actually work now
    yay

--- Scenes/TileMap.gd
+++ Scenes/TileMap.gd
@@ -1,33 +1,34 @@
 extends TileMap
 
 var should_disapear = false
-var layer1_visible = true
+var layer1_turning_visible = true
 var alpha_delta = .06
 
-func _ready():
-	var tiles = get_used_cells(1)
-	for tile in tiles:
-		get_cell_tile_data(1, tile).remove_collision_polygon(0, 0)
+func _use_tile_data_runtime_update(layer: int, coords: Vector2i) -> bool:
+	return layer in [1]
+
+func _tile_data_runtime_update(layer: int, coords: Vector2i, tile_data: TileData) -> void:
+	tile_data.set_collision_polygons_count(0, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if should_disapear and layer1_visible:
+	#Turning invisible
+	if should_disapear and !layer1_turning_visible:
 		var curr_mod = get_layer_modulate(1)
 		curr_mod.a -= alpha_delta
 		set_layer_modulate(1, curr_mod)
 		if get_layer_modulate(1).a < 0:
 			should_disapear = false
-			layer1_visible = false
 			curr_mod.a = 0
 			set_layer_modulate(1, curr_mod)
-
-	elif should_disapear and !layer1_visible:
+			
+	#Turning visible
+	elif should_disapear and layer1_turning_visible:
 		var curr_mod = get_layer_modulate(1)
 		curr_mod.a += alpha_delta
 		set_layer_modulate(1, curr_mod)
 		if get_layer_modulate(1).a > 1:
 			should_disapear = false
-			layer1_visible = true
 			curr_mod.a = 1
 			set_layer_modulate(1, curr_mod)
 
@@ -35,3 +36,4 @@ func _process(_delta):
 
 func _on_squirrel_venting():
 	should_disapear = true
+	layer1_turning_visible = !layer1_turning_visible
