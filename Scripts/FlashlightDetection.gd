7d110e434102f5437eb386f000b12af32d20169f
jtkardon <jtkardon@gmail.com>
2023-11-28 10:24:38 -0500

    exported variables

--- Scripts/FlashlightDetection.gd
+++ Scripts/FlashlightDetection.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-var susLimit = 1000
+@export var susLimit = 1000
+@export var susIncrement = 15
+@export var susDecrement = 5
 var susValue = 0
 
 # Checks if the player has hit the flashlight
@@ -8,7 +10,7 @@ func _on_body_entered(_body):
 	var bottom = $RayCastBottom.is_colliding() and $RayCastBottom.get_collider().is_in_group("player")
 	var top = $RayCastTop.is_colliding() and $RayCastTop.get_collider().is_in_group("player")
 	if  bottom or top:
-		susValue += 15
+		susIncrement += 15
 		if susValue > susLimit:
 			get_parent().get_parent().get_node("Squirrel").set_is_dead(true)
 			await get_tree().create_timer(0.1).timeout
@@ -19,6 +21,6 @@ func _process(_delta):
 	var bodies = get_overlapping_bodies()
 	print(susValue)
 	if susValue > 0:
-		susValue -= 5
+		susDecrement -= 5
 	for i in range(0,bodies.size()):
 		_on_body_entered(bodies[i])
