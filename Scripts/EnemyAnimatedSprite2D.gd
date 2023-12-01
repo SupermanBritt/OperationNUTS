d30971d92af6abb98211c135fff486ba7b545648
SupermanBritt <supermanbritt03@comcast.net>
2023-12-01 11:05:54 -0500

    Simplified code

--- Scripts/EnemyAnimatedSprite2D.gd
+++ Scripts/EnemyAnimatedSprite2D.gd
@@ -2,10 +2,8 @@ extends AnimatedSprite2D
 
 func enableOutline():
 	material.set_shader_parameter("enabled", true)
-	print("its called!")
 
 func disableOutline():
-#	self.material_override.set_shader_parameter("enabled", false)
 	material.set_shader_parameter("enabled", false)
 
 func _process(_delta):
